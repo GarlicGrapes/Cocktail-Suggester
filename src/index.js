c6d872a61e97e2607d8aeb243371532260dbdda5
Austin <austinlongocontracting@gmail.com>
2022-02-11 18:51:16 -0500

    created loop to add 3 cocktails with async function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-const baseUrl = 'https://www.thecocktaildb.com'
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'
 
 window.addEventListener('DOMContentLoaded', () => {
-    getCocktails()
+    generateCocktails()
 })
 
-function getCocktails() {
-//    const ul = document.getElementById('cocktail-list')
+// function getCocktails() {
+//     const ul = document.getElementById('cocktail-list')
     
-    fetchCocktails()
-    .then(data => console.log(data))
-}
+//     fetchCocktails()
+//     .then(data => {
+//         console.log(data)
+//         data.drinks.forEach(drink => { 
+//             if(drink.strCategory === "Shot") {
+//             ul.innerHTML += `<li>${drink.strDrink} is served in a ${drink.strGlass}</li>`
+//             }
+//         })
+//     })
+// }
+
+async function generateCocktails() {
+    const ul = document.getElementById('cocktail-list')
+    let i = 0
+    let o = 0
+    while (i < 3) {
+    await fetchCocktails()
+    .then(data => {
+        
+        if (isCocktail(data.drinks[0].strCategory) === true) {
+            ul.innerHTML += `<li>${data.drinks[0].strDrink}</li>`
+            i ++
+            console.log("i = " + i)
+        }      
+        })
+    o++
+    console.log('o = ' + o)
+    }
+    }
+
+
 
 async function fetchCocktails() {
-    let res = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+    let res = await fetch(baseUrl + 'random.php' )
     let data = await res.json()
     return data
 }
+
+let isCocktail = (bev) => bev === "Cocktail" || bev === "Ordinary Drink"
+
