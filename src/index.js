75e3cdf0731cc74fda541a8541d4cdd4aaa83ed6
Austin <austinlongocontracting@gmail.com>
2022-02-12 02:34:10 -0500

    built drinkDetailDisplay and buildIngredientsList - Ingredients not functional

--- src/index.js
+++ src/index.js
@@ -21,26 +21,64 @@ window.addEventListener('DOMContentLoaded', () => {
 async function generateCocktails() {
     const ul = document.getElementById('cocktail-list')
     let i = 0
-    let o = 0
     while (i < 3) {
-    await fetchCocktails()
+        await fetchCocktails('random.php')
+        .then(data => {
+            if (isCocktail(data.drinks[0].strCategory) === true) {
+                ul.innerHTML += `<li><a href="#" data-id="${data.drinks[0].idDrink}">${data.drinks[0].strDrink}</a></li>`
+                i ++
+                console.log("i = " + i)
+            }      
+            })
+    attachClicksToDrinkList()
+    }
+    }
+
+const attachClicksToDrinkList = () => {
+    const drinks = document.querySelectorAll('a')
+    drinks.forEach((drink) => {
+        drink.addEventListener('click', displayDrinkDetails)
+        console.log(`im adding a listener for ${drink}`)
+    })
+    }
+
+const displayDrinkDetails = (event) => {
+    const drinkId = event.target.dataset.id
+    console.log(event.target.dataset.id)
+    const ul = document.getElementById('cocktail-list')
+    fetchCocktails("lookup.php?i=" + drinkId)
     .then(data => {
-        
-        if (isCocktail(data.drinks[0].strCategory) === true) {
-            ul.innerHTML += `<li>${data.drinks[0].strDrink}</li>`
-            i ++
-            console.log("i = " + i)
-        }      
-        })
-    o++
-    console.log('o = ' + o)
-    }
-    }
+        const drinkPrefix = data.drinks[0]
+        ul.innerHTML = `
+        <img src=${drinkPrefix.strDrinkThumb} alt=Image of ${drinkPrefix.strDrink} height = "25%" width = "25%">
+        <br>    
+        <ul>
+            <li> ${drinkPrefix.strDrink}</li>
+            <li>ingredients: </li>
+            ${buildIngredientsList(drinkPrefix)}
+            <li>${drinkPrefix.strInstructions}</li>
+        `
+    })
+}
 
+function buildIngredientsList(drink) {
+    let list = ""
+    if (drink.strIngredient1 == true) {list += `<li>${drink.strMeasure1} ${drink.strIngredient1}</li>`}
+    if (drink.strIngredient2 == true) {list += `<li>${drink.strMeasure2} ${drink.strIngredient2}</li>`}
+    if (drink.strIngredient3 == true) {list += `<li>${drink.strMeasure3} ${drink.strIngredient3}</li>`}
+    if (drink.strIngredient4 == true) {list += `<li>${drink.strMeasure4} ${drink.strIngredient4}</li>`}
+    if (drink.strIngredient5 == true) {list += `<li>${drink.strMeasure5} ${drink.strIngredient5}</li>`}
+    if (drink.strIngredient6 == true) {list += `<li>${drink.strMeasure6} ${drink.strIngredient6}</li>`}
+    if (drink.strIngredient7 == true) {list += `<li>${drink.strMeasure7} ${drink.strIngredient7}</li>`}
+    if (drink.strIngredient8 == true) {list += `<li>${drink.strMeasure8} ${drink.strIngredient8}</li>`}
+    if (drink.strIngredient9 == true) {list += `<li>${drink.strMeasure9} ${drink.strIngredient9}</li>`}
+    if (drink.strIngredient10 == true) {list += `<li>${drink.strMeasure10} ${drink.strIngredient10}</li>`}
+    return list
+}
 
-
-async function fetchCocktails() {
-    let res = await fetch(baseUrl + 'random.php' )
+async function fetchCocktails(requestType) {
+    let res = await fetch(baseUrl + requestType )
+    console.log(res)
     let data = await res.json()
     return data
 }
