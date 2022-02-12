9c02309dbffee3a4036997cbe0a42ee175daf237
Austin Longo <austinlongocontracting@gmail.com>
2022-02-12 14:59:07 -0500

    updated styles and added reroll button

--- src/index.js
+++ src/index.js
@@ -2,31 +2,18 @@ const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'
 
 window.addEventListener('DOMContentLoaded', () => {
     generateCocktails()
+    buildRerollButton()
 })
-
-// function getCocktails() {
-//     const ul = document.getElementById('cocktail-list')
-    
-//     fetchCocktails()
-//     .then(data => {
-//         console.log(data)
-//         data.drinks.forEach(drink => { 
-//             if(drink.strCategory === "Shot") {
-//             ul.innerHTML += `<li>${drink.strDrink} is served in a ${drink.strGlass}</li>`
-//             }
-//         })
-//     })
-// }
 
 async function generateCocktails() {
     const ul = document.getElementById('cocktail-list')
-    let i = 0
-    while (i < 3) {
+    let addedToList = 0
+    while (addedToList < 3) {
         await fetchCocktails('random.php')
         .then(data => {
             if (isCocktail(data.drinks[0].strCategory) === true) {
                 ul.innerHTML += `<li><a href="#" data-id="${data.drinks[0].idDrink}">${data.drinks[0].strDrink}</a></li>`
-                i ++
+                addedToList ++
             }      
             })
     attachClicksToDrinkList()
@@ -48,28 +35,36 @@ const displayDrinkDetails = (event) => {
         const drinkPrefix = data.drinks[0]
         ul.innerHTML = `
         <img src=${drinkPrefix.strDrinkThumb} alt=Image of ${drinkPrefix.strDrink} height = "25%" width = "25%">
-        <br>    
-        <ul>
-            <li> ${drinkPrefix.strDrink}</li>
-            <li>ingredients: </li>
-            ${buildIngredientsList(drinkPrefix)}
-            <li>${drinkPrefix.strInstructions}</li>
+        <br>
+        <strong>${drinkPrefix.strDrink}</strong>   
+        <br>
+        <br>
+        
+        <strong><u>Ingredients:</u></strong>
+        <br>
+        ${buildIngredientsList(drinkPrefix)}
+        <br>
+        <strong><u>Instructions:</u></strong>
+        <br>
+        ${drinkPrefix.strInstructions}
         `
+
+
     })
 }
 
 const buildIngredientsList = (drink) => {
     let list = ""
-    if (ingredientChecker(drink.strIngredient1) === true) {list += `<li>${drink.strMeasure1} ${drink.strIngredient1}</li>`}
-    if (ingredientChecker(drink.strIngredient2) === true) {list += `<li>${drink.strMeasure2} ${drink.strIngredient2}</li>`}
-    if (ingredientChecker(drink.strIngredient3) === true) {list += `<li>${drink.strMeasure3} ${drink.strIngredient3}</li>`}
-    if (ingredientChecker(drink.strIngredient4) === true) {list += `<li>${drink.strMeasure4} ${drink.strIngredient4}</li>`}
-    if (ingredientChecker(drink.strIngredient5) === true) {list += `<li>${drink.strMeasure5} ${drink.strIngredient5}</li>`}
-    if (ingredientChecker(drink.strIngredient6) === true) {list += `<li>${drink.strMeasure6} ${drink.strIngredient6}</li>`}
-    if (ingredientChecker(drink.strIngredient7) === true) {list += `<li>${drink.strMeasure7} ${drink.strIngredient7}</li>`}
-    if (ingredientChecker(drink.strIngredient8) === true) {list += `<li>${drink.strMeasure8} ${drink.strIngredient8}</li>`}
-    if (ingredientChecker(drink.strIngredient9) === true) {list += `<li>${drink.strMeasure9} ${drink.strIngredient9}</li>`}
-    if (ingredientChecker(drink.strIngredient10) === true) {list += `<li>${drink.strMeasure10} ${drink.strIngredient10}</li>`}
+    if (ingredientChecker(drink.strIngredient1) === true) {list += `${drink.strMeasure1} ${drink.strIngredient1}<br>`}
+    if (ingredientChecker(drink.strIngredient2) === true) {list += `${drink.strMeasure2} ${drink.strIngredient2}<br>`}
+    if (ingredientChecker(drink.strIngredient3) === true) {list += `${drink.strMeasure3} ${drink.strIngredient3}<br>`}
+    if (ingredientChecker(drink.strIngredient4) === true) {list += `${drink.strMeasure4} ${drink.strIngredient4}<br>`}
+    if (ingredientChecker(drink.strIngredient5) === true) {list += `${drink.strMeasure5} ${drink.strIngredient5}<br>`}
+    if (ingredientChecker(drink.strIngredient6) === true) {list += `${drink.strMeasure6} ${drink.strIngredient6}<br>`}
+    if (ingredientChecker(drink.strIngredient7) === true) {list += `${drink.strMeasure7} ${drink.strIngredient7}<br>`}
+    if (ingredientChecker(drink.strIngredient8) === true) {list += `${drink.strMeasure8} ${drink.strIngredient8}<br>`}
+    if (ingredientChecker(drink.strIngredient9) === true) {list += `${drink.strMeasure9} ${drink.strIngredient9}<br>`}
+    if (ingredientChecker(drink.strIngredient10) === true) {list += `${drink.strMeasure10} ${drink.strIngredient10}<br>`}
     return list
 }
 
@@ -84,10 +79,22 @@ let isCocktail = (bev) => bev === "Cocktail" || bev === "Ordinary Drink"
 
 const ingredientChecker = (ingredient) => {
 //checks to see if ingredient exists/has value
-    if (ingredient != undefined){
+    if (ingredient != undefined && ingredient != null){
         if(ingredient.length > 0) {
             return true
         }
     }
     return false
+}
+
+function reroll() {
+    const ul = document.getElementById('cocktail-list')
+    ul.innerHTML = ""
+    generateCocktails()
+
+}
+
+function buildRerollButton() {
+    const button = document.getElementById('reroll')
+    button.addEventListener('click', reroll)
 }
